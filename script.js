2a986d07e72ccf192649b1ec69fc758d9862bff2
Hadryan César do Nascimento Santos <hadryan2007nascimentosantos@gmail.com>
2024-10-11 10:01:19 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,76 @@
+function TrocarPag(caminho) {
+    window.location.href = caminho
+}
+function splitArray(array, size) {
+    const result = [];
+    for (let i = 0; i < array.length; i += size) {
+        result.push(array.slice(i, i + size));
+    }
+    return result;
+}
+function closePopup() {
+    const popup = document.getElementById('popup');
+    popup.style.display = 'none'
+    popup.innerHTML = '<button onclick="closePopup()" class="close-popup" id="closePopup">&times;</button>'
+    document.getElementById('overlay').style.display = 'none';
+}
+function openPopup(tipo) {
+    document.getElementById('popup').style.display = 'flex';
+    contentPopup(tipo)
+    document.getElementById('overlay').style.display = 'block';
+}
+const atividades = [
+    "Exemplo.html",
+]
+const projetos = [
+    "Exemplo.html",
+]
+const trabalhos = [
+    "Exemplo.html",
+]
+function contentPopup(tipo) {
+    if (tipo == "Atividades") {
+        const elementos = contPopupMaker(atividades, tipo)
+        elementos.forEach(element => {
+            document.getElementById('popup').appendChild(element)
+        });
+    }
+    else if (tipo == "Projetos") {
+        const elementos = contPopupMaker(projetos, tipo)
+        elementos.forEach(element => {
+            document.getElementById('popup').appendChild(element)
+        });
+    }
+    else if (tipo == "Trabalhos") {
+        const elementos = contPopupMaker(trabalhos, tipo)
+        elementos.forEach(element => {
+            document.getElementById('popup').appendChild(element)
+        });
+    }
+}
+function contPopupMaker(lista, tipo) {
+    const totalLinks = []
+    for (let i = 0; i < lista.length; i++) {
+        const a = document.createElement("a")
+        a.textContent = `${tipo} ${i + 1}`;
+
+        const divLink = document.createElement("div")
+        divLink.classList.add("div-link")
+        divLink.appendChild(a)
+        divLink.addEventListener("click", function () {
+            window.location.href = lista[i]
+        })
+        totalLinks.push(divLink)
+    }
+    const splitResults = splitArray(totalLinks, 6)
+    const links = []
+    for (let j = 0; j < splitResults.length; j++) {
+        const div = document.createElement("div")
+
+        for (let l = 0; l < splitResults[j].length; l++) {
+            div.appendChild(splitResults[j][l])
+        }
+        links.push(div)
+    }
+    return links
+}
